fbcfe7a336a369f841e7d6c680fe2385f76bcbe8
Aaron Wu <wuaaron12345@gmail.com>
2020-10-14 02:30:59 +0000

    Initial commit

---
+++ script3.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	var loc = window.location.href+'';
+	if (loc.indexOf('http://')==0){
+		window.location.href = loc.replace('http://','https://');	// https redirect
+	}	
+	
+			}			
+		}
+		if(e.which == 13) { //enter
+			window.open("https://win10-gen4.aaronwu7.repl.co/bios","_self");
+		}
+		if(e.which == 112) { //F1
+			
+		}
+		if(e.which == 27) { //Esc
+			
+		}
+		if(e.which == 113) { //F2
+			
+		}
+		if(e.which == 120) { //F9
+			window.open("https://win10-gen4.aaronwu7.repl.co/bios","_self");
+		}		
+}
